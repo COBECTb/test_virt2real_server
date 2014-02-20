f86c167cc7c5baeb5c62448603ac6448b699bba9
COBECTb <sovest@Mac-mini-Aleksandr.local>
2014-02-20 14:01:43 +0400

    Бинарник и скрипт сборки

---
+++ server/make.sh
@@ -0,0 +1,3 @@
+
+/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-gcc -O0 -Wa,-adhlns=mem_util.o.lst -fmessage-length=0 -MMD -MP -MFmem_util.d -MTmem_util.d -mcpu=arm926ej-s -g3 -gdwarf-2 -o server server.c
+
